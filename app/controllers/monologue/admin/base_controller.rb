13b8028cb805fb079a686922296f2dc78debb8a9
jipiboily <jipiboily@gmail.com>
2012-01-15 21:22:44 -0500

    Logout and admin layout

--- app/controllers/monologue/admin/base_controller.rb
+++ app/controllers/monologue/admin/base_controller.rb
@@ -1,6 +1,8 @@
 class Monologue::Admin::BaseController < Monologue::ApplicationController
   before_filter :authenticate_user!
   
+  layout "layouts/monologue/admin"
+  
   def authenticate_user!
     if current_user.nil?
       redirect_to admin_login_url, :alert => "You must first log in to access admin section."
--- spec/integration/sessions_spec.rb
+++ spec/integration/sessions_spec.rb
@@ -9,18 +9,18 @@ describe "sessions" do
     page.should have_content("Logged in!")
   end
   
-  # it "won't log if bad credentials are provided" do
-  #   user = Factory(:user)
-  #   user.password = "wrong"
-  #   visit admin_login_path
-  #   fill_in "E-mail", with: user.email
-  #   fill_in "Password", with: user.password
-  #   click_button "Login"
-  #   page.should have_content("Login failed")
-  # end
-  # 
-  # it "can log out" do
-  #   user = Factory(:user)
-  #   visit 
-  # end
+  it "won't log if bad credentials are provided" do
+    user = Factory(:user)
+    visit admin_login_path
+    fill_in "email", with: user.email
+    fill_in "Password", with: "whatever"
+    click_button "Log in"
+    page.should have_content("Invalid email or password")
+  end
+
+  it "can log out" do
+    user = Factory(:user)
+    visit admin_path
+    click_link "Log out"
+  end
 end
