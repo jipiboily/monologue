d3cb61c534721453ae39dcb9b0b45b5cbf6c6984
jipiboily <jipiboily@gmail.com>
2012-01-19 22:02:29 -0500

    Refactored a bit session's specs

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -1,22 +1,28 @@
 require 'spec_helper'
 describe "sessions" do
-  it "can log if password provided is valid" do
-    log_in
+  context "logged in user" do
+    before(:each) do
+      log_in
+    end
+    
+    it "can log out" do
+      visit admin_path
+      click_link "Log out"
+    end
   end
   
-  it "won't log if bad credentials are provided" do
-    user = Factory(:user)
-    visit admin_login_path
-    fill_in "email", with: user.email
-    fill_in "Password", with: "whatever"
-    click_button "Log in"
-    page.should have_content("Invalid email or password")
-  end
+  context "NOT logged in user" do
+    it "can log if password provided is valid" do
+      log_in
+    end
 
-  it "can log out" do
-    log_in
-    user = Factory(:user)
-    visit admin_path
-    click_link "Log out"
-  end
+    it "won't log if bad credentials are provided" do
+      user = Factory(:user)
+      visit admin_login_path
+      fill_in "email", with: user.email
+      fill_in "Password", with: "whatever"
+      click_button "Log in"
+      page.should have_content("Invalid email or password")
+    end
+  end  
 end
