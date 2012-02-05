b8b8d22340b8692407b2671ccd7e5f74024030cd
jipiboily <jipiboily@gmail.com>
2012-02-05 16:43:45 -0500

    Some basic frontend theming with bootstrap. Also modified datetime format to a date format

--- spec/dummy/config/initializers/monologue.rb
+++ spec/dummy/config/initializers/monologue.rb
@@ -1,4 +1,5 @@
-Monologue.site_name            = "jipiboily"
+Monologue.site_name            = "@jipiboily / Jean-Philippe Boily"
+Monologue.site_subtitle        = "my own place online"
 Monologue.site_url             = "http://jipiboily.com"
 Monologue.disqus_shortname     = "jipiboily"
 Monologue.meta_description     = "This is my personal blog about Rails, Monologue, programming, etc..."
