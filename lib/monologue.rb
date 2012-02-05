18b1b0f5cf3576aa8d19e6f18899cf1a13e82045
jipiboily <jipiboily@gmail.com>
2012-02-05 09:27:55 -0500

    Added comments and configurations (#23 and #22)

--- lib/monologue.rb
+++ lib/monologue.rb
@@ -1,4 +1,8 @@
 require "monologue/engine"
 
 module Monologue
+  mattr_accessor :disqus_shortname,
+                 :site_name,
+                 :meta_description,
+                 :meta_keyword
 end
---
+++ spec/dummy/config/initializers/monologue.rb
@@ -0,0 +1,4 @@
+Monologue.site_name        = "jipiboily"
+Monologue.disqus_shortname = "jipiboily"
+Monologue.meta_description = "This is my personal blog about Rails, Monologue, programming, etc..."
+Monologue.meta_keyword     = "rails, programming, monologue, ruby"
