455f78d414cbe2eaf8694db68dba6661dfbc4f74
jipiboily <jipiboily@gmail.com>
2012-03-30 17:48:21 -0400

    admin is now in /monologue (fix #39)

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Monologue::Engine.routes.draw do
   match "/page/:page", to: "posts#index", as: "posts_page"
   match "/feed" => "posts#feed", as: "feed", defaults: {format: :rss}
   
-  namespace :admin do
+  namespace :admin, path: "monologue" do
     get "/" => "posts#index", as: "" # responds to admin_url and admin_path
     get "logout" => "sessions#destroy"
     get "login" => "sessions#new"
