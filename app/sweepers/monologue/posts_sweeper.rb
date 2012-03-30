df151bd57295283f441d52288c4b87f510921fb4
jipiboily <jipiboily@gmail.com>
2012-03-30 13:09:15 -0400

    sweeper fix vs tests

--- app/sweepers/monologue/posts_sweeper.rb
+++ app/sweepers/monologue/posts_sweeper.rb
@@ -3,8 +3,10 @@ class Monologue::PostsSweeper < ActionController::Caching::Sweeper
 
 
   def sweep(post)
-    all_monologue_cache_dir = ActionController::Base.page_cache_path('/monologue/').gsub(".html","")
-    FileUtils.rm_rf ActionController::Base.page_cache_path('/monologue/')
+    root_path = Monologue::Engine.routes.url_helpers.root_path if root_path.nil? # TODO: why do I have to do this to make tests pass? There must be something much more clean to make tests pass
+    page_cache_directory = Rails.public_path if page_cache_directory.nil? # TODO: we should not need this either...
+    all_monologue_cache_dir =  "#{page_cache_directory}#{root_path}"
+    FileUtils.rm_rf "#{page_cache_directory}#{root_path.chomp("/")}.html"
     FileUtils.rm_rf all_monologue_cache_dir
   end
 
