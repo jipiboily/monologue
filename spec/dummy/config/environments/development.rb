4f98c10ae72b50bbb94af2e5989a7a13977836da
jipiboily <jipiboily@gmail.com>
2012-04-22 20:29:40 -0400

    removed caching config in dummy

--- spec/dummy/config/environments/development.rb
+++ spec/dummy/config/environments/development.rb
@@ -11,7 +11,7 @@ Dummy::Application.configure do
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = true
+  # config.action_controller.perform_caching = false
   config.action_controller.page_cache_directory = Rails.public_path
 
   # Don't care if the mailer can't send
--- spec/dummy/config/environments/test.rb
+++ spec/dummy/config/environments/test.rb
@@ -16,7 +16,7 @@ Dummy::Application.configure do
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = true
+  # config.action_controller.perform_caching = true
   config.action_controller.cache_store = :file_store, Rails.public_path
 
 
