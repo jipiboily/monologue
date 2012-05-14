9f865729ea880dfe895453dc1265b23ed6b85866
Michael Sevestre <michael.sevestre@gmail.com>
2012-05-14 12:38:14 -0400

    Tests are running, starting implementation

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -16,8 +16,8 @@ ActiveRecord::Schema.define(:version => 20120120193907) do
   create_table "monologue_posts", :force => true do |t|
     t.integer  "posts_revision_id"
     t.boolean  "published"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",        :null => false
+    t.datetime "updated_at",        :null => false
   end
 
   create_table "monologue_posts_revisions", :force => true do |t|
@@ -27,8 +27,8 @@ ActiveRecord::Schema.define(:version => 20120120193907) do
     t.integer  "user_id"
     t.integer  "post_id"
     t.datetime "published_at"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
   end
 
   add_index "monologue_posts_revisions", ["id"], :name => "index_monologue_posts_revisions_on_id", :unique => true
@@ -39,8 +39,8 @@ ActiveRecord::Schema.define(:version => 20120120193907) do
     t.string   "name"
     t.string   "email"
     t.string   "password_digest"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
   end
 
 end
