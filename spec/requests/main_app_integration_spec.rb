7ec13d5f3a9e00df8fa5b2740f3732a67f64ae79
jipiboily <jipiboily@gmail.com>
2012-05-17 21:36:54 -0400

    clear cache before each test.
    repeat after me: "clear cache before each integration test". Again: "…"

--- spec/requests/main_app_integration_spec.rb
+++ spec/requests/main_app_integration_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 describe "main_app_integration" do
+  before(:each) do
+    clear_cache
+  end
+  
   it "should use main_app layout" do
     # TODO: make a working test with that. Changing layout on the fly does not seem to work.
     # Monologue.layout = "layouts/application"
