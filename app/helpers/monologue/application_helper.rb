d5013687817c0ee2d002af364fe47dfab927c121
jipiboily <jipiboily@gmail.com>
2012-05-26 11:55:36 -0400

    moved title tag content generation to a helper

--- app/helpers/monologue/application_helper.rb
+++ app/helpers/monologue/application_helper.rb
@@ -4,5 +4,9 @@ module Monologue
       options[:builder] = MonologueAdminFormBuilder
       form_for(object, options, &block)
     end
+
+    def monologue_accurate_title
+      content_for?(:title) ? ((content_for :title) + " | #{Monologue.site_name}") : Monologue.site_name
+    end
   end
 end
