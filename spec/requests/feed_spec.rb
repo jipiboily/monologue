e090ebc2623d5296ac323771b1de243030a0cc69
jipiboily <jipiboily@gmail.com>
2012-06-02 22:41:45 -0400

    fixed regression "RSS URLs are wrong" (#72)

---
+++ spec/requests/feed_spec.rb
@@ -0,0 +1,13 @@
+# encoding: UTF-8
+require 'spec_helper'
+describe "feed" do
+  before(:each) do
+    Factory(:posts_revision, url: "url/to/post")
+  end
+  
+  # test to prevent regression for issue #72
+  it "should contain full" do
+    visit feed_path
+    page.should have_content "/monologue/url/to/post"
+  end
+end
