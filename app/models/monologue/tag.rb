50d693b0b6adc9b6450f1980b026a483db464bcc
Michael Sevestre <michael.sevestre@gmail.com>
2012-06-03 21:30:01 -0400

    Rename join table 'posts_tags' to 'monologue_posts_tags'

--- app/models/monologue/tag.rb
+++ app/models/monologue/tag.rb
@@ -2,7 +2,7 @@ class Monologue::Tag < ActiveRecord::Base
   attr_accessible :name
 
   validates :name, :uniqueness => true,:presence => true;
-  has_and_belongs_to_many :posts
+  has_and_belongs_to_many :posts,:join_table => :monologue_posts_tags
 
   def posts_with_tag
     self.posts.published
