394f211631ae9aafdcef4a06f0f860956baf5b30
Mohammad El-Abid <mohammad.elabid@gmail.com>
2012-07-06 01:10:36 -0400

    append monologue to current_user variables (session and instance) to prevent conflicts with the main application

--- app/controllers/monologue/admin/sessions_controller.rb
+++ app/controllers/monologue/admin/sessions_controller.rb
@@ -7,7 +7,7 @@ class Monologue::Admin::SessionsController < Monologue::Admin::BaseController
   def create
     user = Monologue::User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
+      session[:monologue_user_id] = user.id
       redirect_to admin_url, :notice => t("monologue.admin.sessions.messages.logged_in")
     else
       flash.now.alert = t("monologue.admin.sessions.messages.invalid")
@@ -16,7 +16,7 @@ class Monologue::Admin::SessionsController < Monologue::Admin::BaseController
   end
 
   def destroy
-    session[:user_id] = nil
+    session[:monologue_user_id] = nil
     redirect_to admin_url, :notice => t("monologue.admin.sessions.messages.logged_out")
   end
 end
