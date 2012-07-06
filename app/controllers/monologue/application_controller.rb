25f49d5addde6b52e00cc5108dcccb4f2dcfe04c
Mohammad El-Abid <mohammad.elabid@gmail.com>
2012-07-06 01:10:36 -0400

    append monologue to current_user variables (session and instance) to prevent conflicts with the main application

--- app/controllers/monologue/application_controller.rb
+++ app/controllers/monologue/application_controller.rb
@@ -30,7 +30,7 @@ class Monologue::ApplicationController < ApplicationController
   private
 
     def current_user
-      @current_user ||= Monologue::User.find(session[:user_id]) if session[:user_id]
+      @monologue_current_user ||= Monologue::User.find(session[:monologue_user_id]) if session[:monologue_user_id]
     end
   
   helper_method :current_user
