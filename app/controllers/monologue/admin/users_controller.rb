b5c39119186d9ba9e4e928e8eec052e3b285ef2a
Jean-Philippe Boily <jipiboily@gmail.com>
2012-09-28 07:27:06 -0400

    clean up and fix build
    oops!

--- app/controllers/monologue/admin/users_controller.rb
+++ app/controllers/monologue/admin/users_controller.rb
@@ -5,12 +5,6 @@ class Monologue::Admin::UsersController < Monologue::Admin::BaseController
 
   def update
     @user = current_user
-    logger.debug params.inspect
-    logger.debug params[:user][:password]
-    logger.debug params[:user][:password].class
-    params[:user][:password].delete if params[:user][:password].empty?
-    params[:user][:password_confirmation].delete if params[:user][:password_confirmation].empty?
-    logger.debug params.inspect
     if @user.update_attributes(params[:user])
       flash.notice = "User modified"
     end
