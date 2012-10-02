469efd6feea928f965efa92ddb21bd33e10f28c7
Jean-Philippe Boily <jipiboily@gmail.com>
2012-10-02 08:24:20 -0400

    add list of cached files

--- spec/dummy/config/initializers/monologue.rb
+++ spec/dummy/config/initializers/monologue.rb
@@ -31,3 +31,10 @@ Monologue.google_plus_account_url = "https://plus.google.com/u/1/115273180419164
 Monologue.linkedin_url            = "http://www.linkedin.com/in/jipiboily"
 Monologue.github_username         = "jipiboily"
 Monologue.show_rss_icon           = true
+
+
+# CACHE
+  # ActionController::Base.perform_caching = true
+  # ActionController::Base.page_cache_directory =  = Rails.public_path + "/my-cache-dir"
+Monologue::PageCache.enabled = true
+Monologue::PageCache.wipe_enabled = true
