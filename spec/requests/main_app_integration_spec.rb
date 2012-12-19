8d0d1a61b80eb66781e9d0dc58c41a3472f4fec4
Kyle Bock <kylewbock@gmail.com>
2012-12-19 15:00:26 -0500

    Fixes error if user does not specify sidebars in an initializer
    If the user does not specify an initializer that Contains a
    Monologe.sidebar with an array of sidebars a template error gets
    thrown. Attached is a test that duplcates this error if the
    _sidebar.html.erb template does not have the changes interoduced with
    this commit.

--- spec/requests/main_app_integration_spec.rb
+++ spec/requests/main_app_integration_spec.rb
@@ -10,4 +10,11 @@ describe "main_app_integration" do
     # visit root_path
     # page.should have_content("dummy app layout file!")
   end
+
+  it "should use default layout" do
+    Monologue.sidebar = nil
+    visit root_path
+
+    page.status_code.should be 200
+  end
 end
