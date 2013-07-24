90b3506aec55189697c8a429f59413fcc3667243
Michael Sevestre <michael.sevestre@gmail.com>
2013-07-24 09:09:24 -0400

    Make sure that tag search is not case sensitive. This was causing a problem on postgres databases

--- app/controllers/monologue/tags_controller.rb
+++ app/controllers/monologue/tags_controller.rb
@@ -1,12 +1,19 @@
 class Monologue::TagsController < Monologue::ApplicationController
   caches_page :show , if: Proc.new { monologue_page_cache_enabled? }
   def show
-    @tag = Monologue::Tag.find_by_name(params[:tag])
+    @tag = retrieve_tag
     if @tag
       @page = nil
       @posts = @tag.posts_with_tag
     else
       redirect_to :root ,notice: "No post found with label \"#{params[:tag]}\""
     end
+  end
+
+  private
+  def retrieve_tag
+    tag_name = params[:tag]
+    tag = Monologue::Tag.find_by_name(tag_name)
+    tag ||= Monologue::Tag.select{|t| t.name.downcase == tag_name.downcase}.first
   end
 end
