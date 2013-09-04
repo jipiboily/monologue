6886377e2d79881aab3ce129c60231b8c7884787
Oriol Collell <oriol.collell.bcn@gmail.com>
2013-09-04 23:40:43 +0200

    added spanish locale

---
+++ vendor/assets/javascripts/monologue/bootstrap/bootstrap-datepicker-es.js
@@ -0,0 +1,7 @@
+// Spanish localized datepicker for bootstrap
+$.fn.datepicker.defaults_es = {
+    monthNames: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
+                 "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"],
+    shortDayNames: ["Dom", "Lun", "Mar", "Mie", "Jue", "Vie", "Sab"],
+    startOfWeek: 1
+  };
