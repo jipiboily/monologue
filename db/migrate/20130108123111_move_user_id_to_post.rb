3711227c4fe2e732c052560ec977277e9ad98016
Jean-Philippe Boily <j@jipi.ca>
2013-09-13 07:38:56 -0400

    fix migrations to work in more use cases of upgrades

--- db/migrate/20130108123111_move_user_id_to_post.rb
+++ db/migrate/20130108123111_move_user_id_to_post.rb
@@ -1,9 +1,17 @@
 class MoveUserIdToPost < ActiveRecord::Migration
+  class Monologue::PostsRevision < ActiveRecord::Base
+    attr_accessible :title, :content, :url, :published_at, :post_id
+  end
+
+  class Monologue::Post < ActiveRecord::Base
+    has_many :posts_revisions
+  end
+
   def up
     add_column :monologue_posts, :user_id, :integer
     Monologue::Post.all.each do |post|
       post.user_id = post.posts_revisions.first.user_id
-      post.save!
+      post.save(validate: false)
     end
     remove_column :monologue_posts_revisions, :user_id
   end
