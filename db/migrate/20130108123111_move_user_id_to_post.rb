defab5a13f36bfe44ccad14bac0dfe51f6a81091
Michael Sevestre <michael.sevestre@gmail.com>
2013-09-24 14:28:43 -0400

    Remove usage of attr_accessible in migration

--- db/migrate/20130108123111_move_user_id_to_post.rb
+++ db/migrate/20130108123111_move_user_id_to_post.rb
@@ -1,6 +1,5 @@
 class MoveUserIdToPost < ActiveRecord::Migration
   class Monologue::PostsRevision < ActiveRecord::Base
-    attr_accessible :title, :content, :url, :published_at, :post_id
   end
 
   class Monologue::Post < ActiveRecord::Base
