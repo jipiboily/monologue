19758500e3d214c8146125a7210a016e88b9ffa7
Michael Sevestre <michael.sevestre@gmail.com>
2013-10-20 17:00:02 -0400

    Make authentication method private

--- app/controllers/monologue/controller_helpers/auth.rb
+++ app/controllers/monologue/controller_helpers/auth.rb
@@ -10,6 +10,7 @@ module Monologue
         before_filter :authenticate_user!
       end
 
+      private
       def authenticate_user!
          if monologue_current_user.nil?
            redirect_to monologue.admin_login_url, alert: I18n.t("monologue.admin.login.need_auth")
