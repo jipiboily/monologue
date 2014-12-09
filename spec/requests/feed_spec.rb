2d0273f0aef027954868c10f7a07b34abfd7e801
Andy Brown <asbrown002@gmail.com>
2014-12-09 13:41:35 -0600

    Change single quote to double per style guide

--- spec/requests/feed_spec.rb
+++ spec/requests/feed_spec.rb
@@ -11,25 +11,25 @@ describe "feed" do
     page.should have_content "/monologue/url/to/post"
   end
 
-  context 'with tags param' do
+  context "with tags param" do
     before do
-      create(:post, title: 'Feed Post').tag!(['feed'])
-      create(:post, title: 'Rss Post').tag!(['rss'])
+      create(:post, title: "Feed Post").tag!(["feed"])
+      create(:post, title: "Rss Post").tag!(["rss"])
     end
 
-    context 'with tags' do
-      it 'returns posts tagged with tags' do
-        visit feed_path(tags: 'feed,rss')
-        page.should have_content 'Feed Post'
-        page.should have_content 'Rss Post'
+    context "with tags" do
+      it "returns posts tagged with tags" do
+        visit feed_path(tags: "feed,rss")
+        page.should have_content "Feed Post"
+        page.should have_content "Rss Post"
       end
     end
 
-    context 'without tags' do
-      it 'returns all posts' do
-        visit feed_path(tags: '')
-        page.should have_content 'Feed Post'
-        page.should have_content 'Rss Post'
+    context "without tags" do
+      it "returns all posts" do
+        visit feed_path(tags: "")
+        page.should have_content "Feed Post"
+        page.should have_content "Rss Post"
       end
     end
   end
