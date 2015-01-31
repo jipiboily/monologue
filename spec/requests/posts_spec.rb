bb26bcbf2d12386af37c3c0a873022c760e16dc6
Jake Quain <quainjn@gmail.com>
2015-01-31 11:48:36 -0700

    Eager load users on blog & admin posts#index page

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -5,9 +5,11 @@ describe "posts" do
   end
 
   it "lists posts" do
+    Factory(:post, title: "post Y")
     visit "/monologue"
 
     page.should have_content("post X")
+    page.should have_content("post Y")
   end
 
   it "should route to a post" do
@@ -28,6 +30,12 @@ describe "posts" do
 
   it "should not show post with published date in the future" do
     Factory(:post, published_at: DateTime.new(3000), title: "I am Marty McFly")
+    visit root_path
+    page.should_not have_content "I am Marty McFly"
+  end
+
+  it "should not show an unpublished post" do
+    Factory(:post, published:false, title: "I am Marty McFly")
     visit root_path
     page.should_not have_content "I am Marty McFly"
   end
