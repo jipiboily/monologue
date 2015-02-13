caf2947659ffd370adab45b20d56c1ed257cfff3
Caitlin Goldman <cgoldman@gapintelligence.com>
2015-02-12 23:11:11 -0800

    better aligns config params

--- lib/monologue/configuration.rb
+++ lib/monologue/configuration.rb
@@ -3,35 +3,35 @@ module Monologue
     include ConfigurationExtensions
 
     attr_accessor :disqus_shortname,
-                    :site_name,
-                    :site_subtitle,
-                    :site_url,
-                    :meta_description,
-                    :meta_keyword,
+                  :site_name,
+                  :site_subtitle,
+                  :site_url,
+                  :meta_description,
+                  :meta_keyword,
 
-                    :show_rss_icon,
+                  :show_rss_icon,
 
-                    :twitter_username,
-                    :twitter_locale,
+                  :twitter_username,
+                  :twitter_locale,
 
-                    :facebook_like_locale,
-                    :facebook_url,
-                    :facebook_logo, #used in the open graph protocol to display an image when a post is liked
+                  :facebook_like_locale,
+                  :facebook_url,
+                  :facebook_logo, #used in the open graph protocol to display an image when a post is liked
 
-                    :google_plus_account_url,
-                    :google_plusone_locale,
+                  :google_plus_account_url,
+                  :google_plusone_locale,
 
-                    :linkedin_url,
+                  :linkedin_url,
 
-                    :github_username,
+                  :github_username,
 
-                    :admin_force_ssl,
-                    :posts_per_page,
-                    :admin_posts_per_page,
-                    :google_analytics_id,
-                    :gauge_analytics_site_id,
-                    :layout,
-                    :sidebar
+                  :admin_force_ssl,
+                  :posts_per_page,
+                  :admin_posts_per_page,
+                  :google_analytics_id,
+                  :gauge_analytics_site_id,
+                  :layout,
+                  :sidebar
 
   end
 
