23c987ddb7683cd5b51bf8a3fa142f832816f43a
Wu <superbatironmans5@gmail.com>
2015-04-06 21:50:54 +0800

    add monologue:views generator

---
+++ lib/generators/monologue/views_generator.rb
@@ -0,0 +1,27 @@
+require 'rails/generators/base'
+
+module Monologue
+  module Generators
+    class ViewsGenerator < Rails::Generators::Base
+      desc "Copies Monologue views to your application."
+      argument :scope, required: false, default: nil,
+                       desc: "The scope to copy views to"
+      source_root File.expand_path("../../../../app/views", __FILE__)
+
+      def copy_views
+        view_directory :layouts
+        view_directory :monologue
+      end
+
+      protected
+
+      def view_directory(name)
+        directory name.to_s,"#{target_path}/#{name}" 
+      end
+
+      def target_path
+        @target_path ||= "app/views/#{scope || :monologue}"
+      end
+    end
+  end
+end
