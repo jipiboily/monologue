f4041f3952b25475ca4b6743c49c587cf819395a
JorgeMBP <jorgec@MacBook-Pro-de-JorgeMBP.local>
2015-05-07 10:28:50 +0200

    Improved performance on tag frequency calculation

--- app/models/monologue/tag.rb
+++ app/models/monologue/tag.rb
@@ -7,7 +7,18 @@ class Monologue::Tag < ActiveRecord::Base
     self.posts.published
   end
 
-  def frequency
-    posts_with_tag.size
+  def self.with_frequency
+    self
+      .select("name, COUNT(*) as frequency")
+      .joins("INNER JOIN monologue_taggings ON monologue_taggings.tag_id = monologue_tags.id")
+      .joins("INNER JOIN monologue_posts    ON monologue_posts.id        = monologue_taggings.post_id")
+      .where("monologue_posts.published = 't'")
+      .group("name")
+      .having("COUNT(*) > 0")
   end
+
+  def frequency
+    self.attributes["frequency"].nil? ? posts_with_tag.size : self.attributes["frequency"].to_i
+  end
+  
 end
