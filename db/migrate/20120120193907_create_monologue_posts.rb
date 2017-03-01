d68ca209d9c9be44f673c2ae6403f715dde3bb5d
Omri Cohen <ocohen@gmail.com>
2017-03-01 14:58:28 +0200

    modified migration files

--- db/migrate/20120120193907_create_monologue_posts.rb
+++ db/migrate/20120120193907_create_monologue_posts.rb
@@ -1,10 +1,15 @@
 class CreateMonologuePosts < ActiveRecord::Migration
   def change
     create_table :monologue_posts do |t|
-      t.integer :posts_revision_id
       t.boolean :published
-      
+      t.string :title
+      t.text :content
+      t.string :url
+      t.integer :user_id
+      t.datetime :published_at
       t.timestamps
     end
+
+    add_index :monologue_posts, :url, unique: true
   end
 end
--- db/migrate/20120612020023_add_index_to_tag_name.rb
+++
@@ -1,5 +0,0 @@
-class AddIndexToTagName < ActiveRecord::Migration
-  def change
-    add_index :monologue_tags, :name
-  end
-end
